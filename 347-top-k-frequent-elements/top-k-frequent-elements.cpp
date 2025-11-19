7c92914c7e293a928d385e07584ed270030d7428
Harsh Deep <harshdeephdk@gmail.com>
2025-11-19 17:17:44 +0530

    Time: 3 ms (49.90%) | Memory: 20.1 MB (9.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,25 +1,60 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Optimal (Using Bucket Sort)
         unordered_map<int, int> mpp;
-        vector<int> res;
+        int n = nums.size();
 
         for (int& num : nums) {
             mpp[num]++;
         }
 
-        vector<pair<int, int>> freq(mpp.begin(), mpp.end());
+        vector<vector<int>> bucket(n + 1);
+        // index = freq
+        // value = element
+        // bucket[i] = elements having freq 'i'
 
-        auto comp = [&](auto& a, auto& b) {
-            return a.second > b.second;
-        };
+        for (auto& val : mpp) {
+            int element = val.first;
+            int freq = val.second;
 
-        sort(freq.begin(), freq.end(), comp);
+            bucket[freq].push_back(element);
+        }
 
-        for (int i = 0; i < k; i++) {
-            res.push_back(freq[i].first);
+        vector<int> res;
+
+        for (int i = n; i >= 0; i--) {
+            if (bucket[i].size() == 0)
+                continue;
+
+            while (bucket[i].size() != 0 && res.size() < k) {
+                res.push_back(bucket[i].back());
+                bucket[i].pop_back();
+            }
         }
 
         return res;
+
+        // Brute force
+        // unordered_map<int, int> mpp;
+        // vector<int> res;
+
+        // for (int& num : nums) {
+        //     mpp[num]++;
+        // }
+
+        // vector<pair<int, int>> freq(mpp.begin(), mpp.end());
+
+        // auto comp = [&](auto& a, auto& b) {
+        //     return a.second > b.second;
+        // };
+
+        // sort(freq.begin(), freq.end(), comp);
+
+        // for (int i = 0; i < k; i++) {
+        //     res.push_back(freq[i].first);
+        // }
+
+        // return res;
     }
 };
